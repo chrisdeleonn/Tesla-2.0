fa7ba3588d07c792d49e65cdbec80cefa5c731fa
Chris De Leon <cdeleon.ca91@gmail.com>
2021-09-21 14:08:47 -0400

    added button component & functionality

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import CarItem from "./components/CarItem";
+
 export default function App() {
 	return (
 		<View style={styles.container}>
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { View, Text, ImageBackground } from "react-native";
 import styles from "./styles";
+import StyledButton from "../StyledButton";
+
 const CarItem = (props) => {
 	return (
 		<View style={styles.carContainer}>
@@ -12,6 +14,20 @@ const CarItem = (props) => {
 				<Text style={styles.title}>Model S</Text>
 				<Text style={styles.subtitle}>Starting at $69,420</Text>
 			</View>
+			<StyledButton
+				type="primary"
+				content={"Custom Order"}
+				onPress={() => {
+					console.log("button was pressed");
+				}}
+			/>
+			<StyledButton
+				type="secondary"
+				content={"Existing Inventory"}
+				onPress={() => {
+					console.log("this button was pressed");
+				}}
+			/>
 		</View>
 	);
 };
